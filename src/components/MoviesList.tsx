8a1bb03390734908b950b86f50a97e0311ae8460
Maria Khudyakova <mariahydakova71@gmail.com>
2023-11-04 20:13:16 +0500

    Больше подробностей (часть 1)

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -1,23 +1,37 @@
 import { Film } from '../const.ts';
 import MovieCard from './MovieCard.tsx';
 import { useState } from 'react';
+import { TimeoutId } from '@reduxjs/toolkit/dist/query/core/buildMiddleware/types';
 
 type MoviesListPops = {
   films: Film[];
 };
 
 export default function MoviesList({ films }: MoviesListPops) {
-  const [, setActiveFilm] = useState<string | null>(null);
+  const [activeFilm, setActiveFilm] = useState<string | null>(null);
+  let timer: undefined | TimeoutId = undefined;
 
-  const handleFilmFocus = (id: string | null) => {
-    // console.log('Focus');
-    setActiveFilm(id);
+  const handleFilmFocus = (id: string) => {
+    timer = setTimeout(() => {
+      setActiveFilm(id);
+    }, 1000);
+  };
+
+  const handleFilmOut = () => {
+    clearTimeout(timer);
+    setActiveFilm(null);
   };
 
   return (
     <div className="catalog__films-list">
       {films.map((film) => (
-        <MovieCard film={film} setFunc={handleFilmFocus} key={film.id} />
+        <MovieCard
+          film={film}
+          onMouseOver={handleFilmFocus}
+          onMouseOut={handleFilmOut}
+          activeFilm={activeFilm}
+          key={film.id}
+        />
       ))}
     </div>
   );
---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,54 @@
+import { Film } from '../const.ts';
+import { useEffect, useRef, useState } from 'react';
+
+type VideoPlayerProps = {
+  film: Film;
+  activeFilm: string | null;
+};
+
+export default function VideoPlayer({ film, activeFilm }: VideoPlayerProps) {
+  const playerRef = useRef<HTMLVideoElement | null>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleDataLoaded = () => {
+    setIsLoaded(true);
+  };
+
+  useEffect(() => {
+    const playerElement = playerRef.current;
+    if (!playerElement) {
+      return;
+    }
+
+    playerElement.addEventListener('loadeddata', handleDataLoaded);
+    return () =>
+      playerElement.removeEventListener('loadeddata', handleDataLoaded);
+  }, []);
+
+  useEffect(() => {
+    const playerElement = playerRef.current;
+
+    if (!playerElement || !isLoaded) {
+      return;
+    }
+
+    if (activeFilm === film.id) {
+      playerElement.play();
+      return;
+    }
+
+    playerElement.pause();
+    playerElement.src = film.previewVideoLink;
+  }, [activeFilm, film.id, film.previewVideoLink, isLoaded]);
+
+  return (
+    <video
+      width="280"
+      height="175"
+      poster={film.previewImage}
+      ref={playerRef}
+      src={film.previewVideoLink}
+      muted
+    />
+  );
+}
